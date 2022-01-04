f1eba8e5749807cb8e3dc8a7947291e8f34d941e
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-04 17:34:59 +0100

    Update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React from 'react';
 
 export class MovieView extends React.Component {
-  render(){
-    const {movieData} = this.props;
-    return <div className="movie-view">
-      <div className="movie-poster">
-        <img src={movieData.ImagePath}></img>
+  render() {
+    const { movie, onBackClick } = this.props;
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath}></img>
+        </div>
+        <div className="movie-title">
+          <span className="label">Title:</span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description:</span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          BACK
+        </button>
       </div>
-      <div className="movie-title">
-        <span className="label">Title:</span>
-        <span className="value">{movieData.Title}</span>
-      </div>
-      <div className="movie-description">
-        <span className="label">Description:</span>
-        <span className="value">{movieData.Description}</span>
-      </div>
-    </div>
+    );
   }
-} 
+}
