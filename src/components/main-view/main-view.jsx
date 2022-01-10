226bfa03554d44ad588a89432c50d25deda4d7eb
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-10 19:12:01 +0100

    delete hard coded movie data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,30 +8,7 @@ class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Tron',
-          Description:
-            'Tron is a cult movie for programmers. Jeff Bridges plays the programmer and video game developer  Kevin Flynn who gets sucked into the virtual world of computers. He uses some amazing coding skills to find his way back into the normal world.',
-          ImagePath: '../img/tron_poster.jpeg',
-        },
-        {
-          _id: 2,
-          Title: 'Hackers',
-          Description:
-            'This crime story is starring Angelina Jolie in her first leading role in a major film. It follows a group of high school students who use their programming skills for corporate extortion.',
-          ImagePath:
-            'https://upload.wikimedia.org/wikipedia/en/6/67/Hackersposter.jpg',
-        },
-        {
-          _id: 3,
-          Title: 'The Pirates of Silicon Valley',
-          Description:
-            'This is a must watch movie for everyone who wants to know how personal computers came into the world. The biographical drama tells how Steve Jobs and Bill Gates came up with the idea of creating a computer that could be used by everyone and shows the methodologies they used. It also explores the impact that the rivalry between Jobs and Gates had on the development of the personal computer.',
-          ImagePath: '../img/pirates_of_silicon_valley.jpeg',
-        },
-      ],
+      movies: [],
       selectedMovie: null,
     };
   }
