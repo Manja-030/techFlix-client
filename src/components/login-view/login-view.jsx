0d40099efbdae77dd0067166882b2cdb66816dff
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-13 23:01:50 +0100

    allow user to automatically be logged in
    For testing purpose only.
    Proper authentication procedure will be set later.

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,7 +9,7 @@ function loginView(props) {
     console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    // props.onLoggedIn(username);
+    props.onLoggedIn(username);
   };
 
   return (
