2d2211c4b912efd0bea475a7b6df294acb6bf9c4
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-13 23:20:08 +0100

    debugging

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export function loginView(props) {
+export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
