cd877c17829b7e1c4ab6763e5714d0350c76bdf4
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-15 17:57:42 +0100

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -12,7 +12,7 @@ export class MovieCard extends React.Component {
         }}
       >
         <div>{movie.Title}</div>
-        <div>{movie.ImagePath}</div>
+        <div>{movie.ImagePath}</div
       </div>
     );
   }
@@ -21,7 +21,7 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
-    ImageUrl: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
