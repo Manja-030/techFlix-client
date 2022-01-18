5b11e749042fcbb0ed0da30af6f1bd8655dbbf3a
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-18 16:37:28 +0100

    add style

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, CardTitle, CardBody, Row, Col } from 'react-bootstrap';
+
+import './movie-view.scss';
 
 export class MovieView extends React.Component {
   render() {
     const { movie, onBackClick } = this.props;
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} crossOrigin="true"></img>
-        </div>
-        <div className="movie-title">
-          <span className="label">Title:</span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description:</span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <button
-          onClick={() => {
-            onBackClick(null);
-          }}
-        >
-          BACK
-        </button>
-      </div>
+      <>
+        <Row>
+          <Col>
+            <h2 className="movie-title mb-5">{movie.Title}</h2>
+          </Col>
+        </Row>
+        <Row className="mb-4">
+          <Col md={6} lg={4}>
+            <img
+              className="movie-view"
+              src={movie.ImagePath}
+              alt={movie.Title}
+              crossOrigin="true"
+            />
+          </Col>
+          <Col md={6} lg={4} className="movie-details">
+            {movie.Description}
+          </Col>
+          <Col md={6} lg={4} className="detail-links">
+            <p>Director: </p>
+            <p>Genre:</p>
+            <p>Released: {movie.ReleaseYear}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="add-fav">Add to Favorites</Col>
+          <Col>
+            <button
+              className="back-button"
+              onClick={() => {
+                onBackClick(null);
+              }}
+            >
+              BACK
+            </button>
+          </Col>
+        </Row>
+      </>
     );
   }
 }
@@ -34,6 +55,7 @@ MovieView.propTypes = {
     Title: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
+    ReleaseYear: PropTypes.string.isRequired,
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
