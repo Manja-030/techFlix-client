11e35fa4413f3b1673283dfa93b99eed9f537292
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-20 13:24:09 +0100

    Update getMovies method

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,7 +22,9 @@ class MainView extends React.Component {
 
   componentDidMount() {
     axios
-      .get('https://tech-and-popcorn.herokuapp.com/movies')
+      .get('https://tech-and-popcorn.herokuapp.com/movies', {
+        headers: { Authorization: `Bearer${token}` },
+      })
       .then((response) => {
         this.setState({ movies: response.data });
       })
@@ -40,10 +42,14 @@ class MainView extends React.Component {
 
   /* When a user successfully logs in, this function updates the `user` property in state to that user*/
 
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user,
+      user: authData.user.Username,
     });
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   onRegistered(user) {
