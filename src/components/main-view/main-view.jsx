2f9b929faddf765cf9b89ade8b0d0364dfaa2aaa
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-21 12:19:10 +0100

    add state-based router

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Row, Col, Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { GiPopcorn } from 'react-icons/gi';
 import './main-view.scss';
