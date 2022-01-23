b03e1459201e0873d9610836f4eccccc056e282d
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-23 17:10:29 +0100

    add missing }

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,21 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './director-view.scss';
 
 import { Accordion, Card, Button } from 'react-bootstrap';
 
 function DirectorView({ movie }) {
+	const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const handleOnClick = () => {
-        setIsCollapsed(!isCollapsed)
+	const handleOnClick = () => {
+		setIsCollapsed(!isCollapsed);
+	};
 
 	return (
 		<div className="App">
 			<Accordion defaultActiveKey="0">
 				<Card>
 					<Card.Header>
-						<Accordion.Toggle as={Button} variant="link" eventKey="0" className="p-0" onClick={handleOnClick}>
+						<Accordion.Toggle
+							as={Button}
+							variant="link"
+							eventKey="0"
+							className="p-0"
+							onClick={handleOnClick}
+						>
 							{movie.Director.Name}
 						</Accordion.Toggle>
 					</Card.Header>
