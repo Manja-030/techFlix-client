87812e2d39c22cb9e9c7ca274bdf7915388bf3e2
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-01-26 11:41:31 +0100

    change Accordion

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,37 +1,26 @@
 import React, { useState } from 'react';
 import './genre-view.scss';
+import { BsFillArrowDownCircleFill } from 'react-icons/bs';
+import { BsFillArrowUpCircleFill } from 'react-icons/bs';
 
-import { Accordion, Card, Button } from 'react-bootstrap';
+import { Accordion, Button } from 'react-bootstrap';
 
 function GenreView({ genre }) {
 	const [isCollapsed, setIsCollapsed] = useState(false);
 
-	const handleOnClick = () => {
+	const handleClick = () => {
 		setIsCollapsed(!isCollapsed);
 	};
 
 	return (
-		<div className="App">
-			<Accordion defaultActiveKey="0">
-				<Card>
-					<Card.Header>
-						<Accordion.Toggle
-							as={Button}
-							variant="link"
-							eventKey="0"
-							className="p-0"
-							onClick={handleOnClick}
-						>
-							{genre.Name}
-						</Accordion.Toggle>
-					</Card.Header>
-
-					<Accordion.Collapse eventKey="0">
-						<Card.Body>genre.description</Card.Body>
-					</Accordion.Collapse>
-				</Card>
-			</Accordion>
-		</div>
+		<Accordion>
+			<Accordion.Item eventKey="0">
+				<Accordion.Header as={Button} variant="link" onClick={handleClick}>
+					{genre.Name} {isCollapsed ? <BsFillArrowUpCircleFill /> : <BsFillArrowDownCircleFill />}
+				</Accordion.Header>
+				<Accordion.Body>{genre.Description}</Accordion.Body>
+			</Accordion.Item>
+		</Accordion>
 	);
 }
 
