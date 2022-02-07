404abc7396a5b2e10fd759de711ca607c07b2952
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-02-07 22:25:16 +0800

    beautify code

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -5,48 +5,58 @@ import './navigation.scss';
 import { GiPopcorn } from 'react-icons/gi';
 
 function Navigation({ props }) {
-	const user = localStorage.getItem('user');
+  const user = localStorage.getItem('user');
 
-	const onLoggedOut = () => {
-		localStorage.clear();
-		window.open('/', '_self');
-	};
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open('/', '_self');
+  };
 
-	const isAuth = () => {
-		if (typeof window == 'undefined') {
-			return false;
-		}
-		if (localStorage.getItem('token')) {
-			return localStorage.getItem('token');
-		} else {
-			return false;
-		}
-	};
+  const isAuth = () => {
+    if (typeof window == 'undefined') {
+      return false;
+    }
+    if (localStorage.getItem('token')) {
+      return localStorage.getItem('token');
+    } else {
+      return false;
+    }
+  };
 
-	return (
-		<Navbar id="main-nav" className="mb-5" bg="dark" variant="dark" expand="lg" sticky="top">
-			{/*<Container>*/}
-			<Navbar.Brand id="appName" href="/">
-				<GiPopcorn />
-				techFlix
-			</Navbar.Brand>
+  return (
+    <Navbar
+      id="main-nav"
+      className="mb-5"
+      bg="dark"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+    >
+      {/*<Container>*/}
+      <Navbar.Brand id="appName" href="/">
+        <GiPopcorn />
+        techFlix
+      </Navbar.Brand>
 
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-				<Nav>
-					{isAuth() && <Nav.Link href={`/users/${user}`}>MyPage{user}</Nav.Link>}
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse
+        className="justify-content-end"
+        id="responsive-navbar-nav"
+      >
+        <Nav>
+          {isAuth() && <Nav.Link href={`/users/${user}`}>MyPage</Nav.Link>}
 
-					{isAuth() && (
-						<Button
-							variant="outline-danger"
-							onClick={() => {
-								onLoggedOut();
-							}}
-						>
-							LogOut
-						</Button>
-					)}
-					{/*{!isAuth() && (
+          {isAuth() && (
+            <Button
+              variant="outline-danger"
+              onClick={() => {
+                onLoggedOut();
+              }}
+            >
+              LogOut
+            </Button>
+          )}
+          {/*{!isAuth() && (
 						<Nav.Link className="navbar-link" href={`/`}>
 							LogIn
 						</Nav.Link>
@@ -57,7 +67,7 @@ function Navigation({ props }) {
 						</Nav.Link>
 					)}*/}
 
-					{/*
+          {/*
 
 
 						<Nav.Link id="link" href="">
@@ -79,11 +89,11 @@ function Navigation({ props }) {
 								LogOut
 							</Button>
 						</Nav.Link>*/}
-				</Nav>
-			</Navbar.Collapse>
-			{/*</Container>*/}
-		</Navbar>
-	);
+        </Nav>
+      </Navbar.Collapse>
+      {/*</Container>*/}
+    </Navbar>
+  );
 }
 
 export default Navigation;
