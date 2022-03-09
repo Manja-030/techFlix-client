2c62a4e07b4570cc97fd68f7be3fe19d671d4bba
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-09 22:45:38 +0100

    try to debug movie view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -30,11 +30,75 @@ class MainView extends React.Component {
     });
   }
 
+<<<<<<< Updated upstream
+=======
+  /* When a user successfully logs in, this function updates the `user` property in state to that user*/
+
+  onLoggedIn(authData) {
+    console.log(authData);
+    this.setState({
+      user: authData.user.Username,
+    });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
+    //this.getGenres(authData.token); ** I don't think I need this  **
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null,
+    });
+  }
+
+  onRegistered(user) {
+    this.setState({
+      user,
+    });
+  }
+  //https://tech-and-popcorn.herokuapp.com
+  getMovies(token) {
+    axios
+      .get('http://localhost:8080/movies', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  // ** I don't think I need this piece of code: **
+  /* getGenres(token) {
+    axios
+      .get('https://tech-and-popcorn.herokuapp.com/genres', {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // Assign the result to the state
+        this.setState({
+          genres: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }*/
+
+>>>>>>> Stashed changes
   render() {
     const { movies, selectedMovie } = this.state;
 
     if (movies.length === 0) return <div className="main-view" />;
 
+<<<<<<< Updated upstream
     return (
       <div className="main-view">
         {selectedMovie ? (
@@ -51,6 +115,119 @@ class MainView extends React.Component {
               movie={movie}
               onMovieClick={(movie) => {
                 this.setSelectedMovie(movie);
+=======
+                return movies.map((m) => (
+                  <Col
+                    sm={12}
+                    md={6}
+                    lg={3}
+                    key={m._id}
+                    className="movie-cards"
+                  >
+                    <MovieCard movie={m} />
+                  </Col>
+                ));
+              }}
+            />
+            <Route
+              path="/register"
+              render={() => {
+                if (user) return <Redirect to="/" />;
+                return (
+                  <Col>
+                    <RegistrationView />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              path={`/users/${user}`}
+              render={({ history }) => {
+                if (!user) return <Redirect to="/" />;
+                return (
+                  <Col>
+                    <ProfileView
+                      user={user}
+                      onBackClick={() => history.goBack()}
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              exact
+              path="/movies/:movieId"
+              render={({ match, history }) => {
+                console.log('Match-Params:', match.params);
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                      ;
+                    </Col>
+                  );
+                return (
+                  <Col md={8}>
+                    <MovieView
+                      /*movie={movies.find(
+                        (movie) => movie._id === match.params.movieId
+                      )}*/
+                      movieId={match.params.movieId}
+                      onBackClick={() => history.goBack()}
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              exact
+              path="/director/:Name"
+              render={({ match }) => {
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                      ;
+                    </Col>
+                  );
+                if (movies.length === 0) return <div className="main-view" />;
+                return (
+                  <Col md={8}>
+                    <DirectorView
+                      director={
+                        movies.find(
+                          (movie) => movie.Director.Name === match.params.name
+                        ).Director
+                      }
+                    />
+                  </Col>
+                );
+              }}
+            />
+            <Route
+              exact
+              path="/genres/:id"
+              render={({ match }) => {
+                if (!user)
+                  return (
+                    <Col>
+                      <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+                      ;
+                    </Col>
+                  );
+                if (movies.length === 0) return <div className="main-view" />;
+                return (
+                  <Col md={8}>
+                    <GenreView
+                      genre={
+                        movies.find((movie) =>
+                          movie.Genre.includes(match.params.id)
+                        ).Genre
+                      }
+                    />
+                  </Col>
+                );
+>>>>>>> Stashed changes
               }}
             />
           ))
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,76 @@
 import React from 'react';
+<<<<<<< Updated upstream
+=======
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { Card, CardTitle, CardBody, Row, Col, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import DirectorView from '../director-view/director-view';
+import GenreView from '../genre-view/genre-view';
+import './movie-view.scss';
+import { GiPopcorn } from 'react-icons/gi';
+
+class MovieView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    this.getMovie(accessToken);
+  }
+  /*
+  handleAdd() {
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('user');
+    axios
+      .post(
+        `https://tech-and-popcorn.herokuapp.com/users/${user}` +
+          '/favorites/' +
+          this.props.movie._id,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        console.log(response);
+        alert(
+          this.props.movie.Title + ' has been added to your Favorites List.'
+        );
+        this.props.setUser(response.data);
+        alert('HELLO');
+      });
+  }
+  */
+  getMovie(token) {
+    const { movieId } = this.props;
+    axios
+      .get('http://localhost:8080/movies' + movieId, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log('Axios:', response.data);
+        // Assign the result to the state
+        this.setState({
+          movie: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+>>>>>>> Stashed changes
 
 export class MovieView extends React.Component {
   render() {
+<<<<<<< Updated upstream
     const { movie, onBackClick } = this.props;
+=======
+    const { movie } = this.state;
+>>>>>>> Stashed changes
     return (
       <div className="movie-view">
         <div className="movie-poster">
