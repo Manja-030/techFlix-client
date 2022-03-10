9f9c58f17f49abec76b46b470dc1298fdd9a7d25
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-10 16:01:55 +0100

    Update profile-view.jsx

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { Row, Col, Card, Button, Container } from 'react-bootstrap';
@@ -7,7 +8,8 @@ import Form from 'react-bootstrap/Form';
 import './profile-view.scss';
 import axios from 'axios';
 
-function ProfileView({ user, movies }) {
+function ProfileView({ movies, logOut }) {
+  console.log('movies prop that is imported from MainView:', movies);
   const localUsername = localStorage.getItem('user'); // real username to make axios requests
   const token = localStorage.getItem('token'); // jwt token to make axios requests
 
@@ -16,125 +18,140 @@ function ProfileView({ user, movies }) {
   const [password, setPassword] = useState('');
   const [birthday, setBirthday] = useState('');
 
+  //objects that include error messages as a result of validateChanges
   const [usernameError, setUsernameError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [birthdayError, setBirthdayError] = useState('');
 
-  const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]); //array of movie objects that where added to the FavoriteMovies array
+  const [favIds, setFavIds] = useState([]); //array of movie IDs that where added to FavoriteMovies
 
-  console.log(`https://tech-and-popcorn.herokuapp.com/users/${localUsername}`);
-  console.log(`https://tech-and-popcorn.herokuapp.com/users/${user}`);
+  //convert array of IDs into array of movie objects
+  const getFavs = (favs) => {
+    let favoriteMovieList = [];
+    movies.forEach((movie) => {
+      favs.includes(movie._id) ? favoriteMovieList.push(movie) : null;
+    });
+    setFavorites(favoriteMovieList);
+  };
 
-  /* When component renders for the first time, this fetches user data */
+  //When component renders for the first time, this fetches user data
   useEffect(() => {
     axios
       .get(`https://tech-and-popcorn.herokuapp.com/users/${localUsername}`, {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        console.log(response.data);
+        console.log('response.data:', response.data);
         setUsername(response.data.Username);
         setEmail(response.data.Email);
         setPassword(response.data.Password);
         setBirthday(response.data.Birthday);
-        console.log(response.data.Username);
-        console.log(response.data.Email);
-        console.log(username);
-        console.log(email);
-        console.log(password);
-        console.log(birthday);
+        setFavIds(response.data.FavMovies);
       })
       .catch((e) => {
         console.log(e);
       });
   }, []);
 
+  // When favIds exist, then convert to array of movie objects
   useEffect(() => {
-    getFavs(user.FavMovies);
-  }, []);
+    favIds ? getFavs(favIds) : setFavorites({});
+    console.log('favs in favIds: ' + favIds);
+  }, [favIds]);
 
-  /* converts Array of IDs into Object of movies */
-  const getFavs = (favs) => {
-    let favMovieList = [];
-    movies.forEach((movie) => {
-      favs.includes(movie._id) ? favMovieList.push(movie) : favMovieList;
-    });
-    setFavorites(favMovieList);
-  };
-
-  const validateChanges = () => {
-    let isReq = true;
-
-    if (!username) {
-      setUsernameError('Username is required (at least 4 characters).');
-      isReq = false;
-    } else if (username.length < 4) {
-      setUsernameError('Username must be at least 4 characters long.');
-      isReq = false;
-    }
-
-    if (!password) {
-      setPasswordError('Password is required (at least 6 characters).');
-      isReq = false;
-    } else if (password.length < 6) {
-      setPasswordError('Password must be at least 6 characters long.');
-      isReq = false;
-    }
-
-    if (!email) {
-      setEmailError('Email is required.');
-      isReq = false;
-    } else if (email.indexOf('@') === -1) {
-      setEmailError('Email is not valid.');
-      isReq = false;
-    }
-
-    if (!birthday) {
-      setBirthdayError('Please enter your birthday.');
-      isReq = false;
-    }
-
-    return isReq;
-  };
-
+  // Event handler for updating user profile
   const handleUpdateProfile = (e) => {
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    const isReq = validateChanges();
-    if (isReq) {
+    console.log(
+      'username, password, email, birthday:',
+      username,
+      password,
+      email,
+      birthday
+    );
+    let isValid = validateChanges();
+    if (isValid) {
       axios
         .put(
-          'https://tech-and-popcorn.herokuapp.com/users/${user.Username}',
+          'https://tech-and-popcorn.herokuapp.com/users/${username}',
           {
-            Username: user.Name,
-            Password: user.Password,
-            Email: user.Email,
-            Birthday: user.Birthday,
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthday: birthday,
           },
           { headers: { Authorization: `Bearer ${token}` } }
         )
         .then((response) => {
-          const data = response.data;
-          console.log(data);
+          console.log('response.data:', response.data);
+          console.log('Username:', username);
           alert('Update of profile successful.');
         })
-        .catch((response) => {
-          console.error(response);
+        .catch((error) => {
+          console.log(username);
+          console.error('updating profile not successful:' + error);
           alert('Update of profile not successful.');
         });
     }
   };
-
-  const handleAccountDelete = () => {
+  //delete favorite from list of favorites:
+  const deleteFav = (favId) => {
     axios
-      .delete('https://tech-and-popcorn.herokuapp.com/users/${user.Username}', {
+      .delete(
+        `https://tech-and-popcorn.herokuapp.com/users/${username}/movies/${favId}`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        setFavIds(response.data.FavMovies);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  // event handler to delete user profile
+  const handleProfileDelete = () => {
+    axios
+      .delete('https://tech-and-popcorn.herokuapp.com/users/${username}', {
         headers: { Authorization: `Bearer ${token}` },
       })
       .then((response) => {
-        console.info(response);
+        console.log('Response after Delete', response.data);
+        alert('Profile deleted.');
         logOut();
+      })
+      .catch((error) => {
+        console.log('Error deleting the user', error);
       });
+  };
+
+  // function validates inputs in profile form:
+  const validateChanges = () => {
+    let isValid = true;
+
+    if (username.length < 4) {
+      setUsernameError('Username must have at least 4 characters.');
+      isValid = false;
+    }
+
+    if (password.length < 6) {
+      setPasswordError('Password must have at least 6 characters.');
+      isValid = false;
+    }
+
+    if (email.indexOf('@') === -1) {
+      setEmailError('Email is not valid.');
+      isValid = false;
+    }
+
+    if (!birthday) {
+      setBirthdayError('Please enter your birthday.');
+      isValid = false;
+    }
+
+    return isValid;
   };
 
   return (
@@ -149,16 +166,14 @@ function ProfileView({ user, movies }) {
       </Row>
       <Row>
         <Col>
-          <p>Profile Form to update</p>
           <Form className="profile-form">
             <Form.Group className="mb-3">
               <Form.Label className="profile-text">Username:</Form.Label>
               <Form.Control
                 type="text"
-                defaultValue={user.Username}
-                onChange={(e) => validateChanges(e.target.value)}
-                required
-                placeholder="Enter a username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Enter username"
               />
               <div className="profile-text">
                 {usernameError && <p> {usernameError}</p>}
@@ -168,9 +183,8 @@ function ProfileView({ user, movies }) {
               <Form.Label className="profile-text">Password:</Form.Label>
               <Form.Control
                 type="password"
-                defaultValue={user.Password}
-                onChange={(e) => validateChanges(e.target.value)}
-                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter your password"
               />
               <div className="profile-text">
@@ -181,66 +195,75 @@ function ProfileView({ user, movies }) {
               <Form.Label className="profile-text">Email:</Form.Label>
               <Form.Control
                 type="email"
-                defaultValue={user.Email}
-                onChange={(e) => validateChanges(e.target.value)}
-                required
-                placeholder="Enter your email address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Enter email address"
               />
               <div className="profile-text">
                 {emailError && <p> {emailError}</p>}
               </div>
             </Form.Group>
-
             <Form.Group className="mb-3">
               <Form.Label className="profile-text">Birthday:</Form.Label>
               <Form.Control
-                type="date"
-                defaultValue={user.Birthday}
-                onChange={(e) => validateChanges(e.target.value)}
+                type="text"
+                value={birthday}
+                placeholder="yyyy-mm-dd"
+                onChange={(e) => setBirthday(e.target.value)}
               />
               <div className="profile-text">
                 {birthdayError && <p> {birthdayError}</p>}
               </div>
-            </Form.Group>
-
+            </Form.Group>{' '}
+            <Link to="/profile">
+              <Button
+                type="submit"
+                className="profile-btn"
+                variant="outline-danger"
+                onClick={handleUpdateProfile}
+              >
+                Save Changes
+              </Button>
+            </Link>
             <Button
               type="Submit"
               className="profile-btn"
               variant="outline-danger"
-              onClick={handleUpdateProfile}
-            >
-              Save Changes
-            </Button>
-            <Button
-              type="Submit"
-              className="profile-btn"
-              variant="outline-danger"
-              onClick={handleAccountDelete}
+              onClick={handleProfileDelete}
             >
               Delete Account
             </Button>
           </Form>
         </Col>
+
         <Col>
-          <p>Movieposter of Favorite Movies and Remove-Buttons</p>
+          {' '}
+          {/* This returns in error in console: favorites.map is not a function: {favorites.map((fav) => {
+            return (
+              <div key={fav._id}>
+                <MovieCard movie={fav} />;<Button>Remove</Button>
+              </div>
+            );
+          })}*/}
         </Col>
       </Row>
     </Container>
   );
-}
 
-ProfileView.propTypes = {
-  Username: PropTypes.shape({
-    Favorites: PropTypes.arrayOf(
+  ProfileView.propTypes = {
+    movies: PropTypes.arrayOf(
       PropTypes.shape({
-        _id: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string,
         Title: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+        Genre: PropTypes.array,
+        Director: PropTypes.shape({
+          Name: PropTypes.string,
+          Bio: PropTypes.string,
+        }),
       })
     ),
-    Username: PropTypes.string.isRequired,
-    Email: PropTypes.string.isRequired,
-    Birthday: PropTypes.string,
-  }),
-};
-
+    logOut: PropTypes.func.isRequired,
+  };
+}
 export default ProfileView;
