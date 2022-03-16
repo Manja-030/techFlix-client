94e794b416594e2dcce85eafb699555d663d70fe
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-16 22:04:17 +0100

    create store

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from "./components/main-view/main-view";
+import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import movieApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(movieApp, devToolsEnhancer());
 
 // Main component - will eventually use all the others
 class TechFlixApplication extends React.Component {
   render() {
     return (
-      <div className="tech-flix">
-        <MainView/>
-      </div>
+      <Provider store={store}>
+        <Container>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
