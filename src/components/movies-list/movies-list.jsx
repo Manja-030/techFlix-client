49bd4d773b189e4790f44af5738f9c44e8effe1e
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-17 09:10:15 +0100

    add VisibilityFilter component

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Col, Container } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import MovieCard from '../movie-card/movie-card';
 
 const mapStateToProps = (state) => {
@@ -21,11 +22,18 @@ function MoviesList(props) {
 
   if (!movies) return <div className="main-view" />;
 
-  return filteredMovies.map((movie) => (
-    <Col md={3} key={movie._id}>
-      <MovieCard movie={movie} />{' '}
-    </Col>
-  ));
+  return (
+    <Container>
+      <Col md={12} style={{ margin: '1em' }}>
+        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+      </Col>
+      {filteredMovies.map((movie) => (
+        <Col md={3} key={movie._id}>
+          <MovieCard movie={movie} />
+        </Col>
+      ))}
+    </Container>
+  );
 }
 
 export default connect(mapStateToProps)(MoviesList);
