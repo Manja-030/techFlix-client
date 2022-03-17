6314898a58e493919eb134af79e95546325b6914
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-03-17 15:43:24 +0100

    add SET_USER

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,5 +1,17 @@
+/* // ACTIONS // */
+
+/* Movies */
+
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+
+/* Users */
+
+export const SET_USER = 'SET_USER';
+
+/* // ACTION CREATORS // */
+
+/* Movies */
 
 export function setMovies(value) {
   return {
@@ -11,6 +23,15 @@ export function setMovies(value) {
 export function setFilter(value) {
   return {
     type: SET_FILTER,
+    value,
+  };
+}
+
+/* USERS */
+
+export function setUser(value) {
+  return {
+    type: SET_USER,
     value,
   };
 }
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,4 +1,4 @@
-import { SET_MOVIES, SET_FILTER } from '../actions/actions';
+import { SET_MOVIES, SET_FILTER, SET_USER } from '../actions/actions';
 import { combineReducers } from 'redux';
 
 function movies(state = [], action) {
@@ -19,9 +19,19 @@ function visibilityFilter(state = '', action) {
   }
 }
 
+function user(state = null, action) {
+  switch (action.type) {
+    case SET_USER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
 const movieApp = combineReducers({
   movies,
   visibilityFilter,
+  user,
 });
 
 export default movieApp;
