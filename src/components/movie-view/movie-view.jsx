d81825589191d9c76f7eb84f0cdc2b9424a6d6ed
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-04-28 21:20:11 +0200

    import useEffect

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { changeFavorites } from '../../actions/actions';
