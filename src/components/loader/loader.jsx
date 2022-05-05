845725ad5f51e4b98e0d72f16c9c6903052bdb71
Manja-030 <80778632+Manja-030@users.noreply.github.com>
2022-05-05 17:50:18 +0200

    align spinner

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -3,7 +3,7 @@ import { Spinner } from 'react-bootstrap';
 
 export default function Loader() {
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="d-flex justify-content-center mt-4">
       <Spinner animation="border" variant="danger" />
     </div>
   );
